56785aa39b38c0259f2c21d783da1593777e7c79
Sakuto <vigneronlucweb@gmail.com>
2022-09-22 10:49:05 +0200

    missing key

--- pages/api/axios.js
+++ pages/api/axios.js
@@ -1,19 +1,20 @@
 import axios from 'axios'
 
 const url = 'https://api.rawg.io';
+const API_KEY = cd68cf822b8b4b59adf1c46ac1ea961c
 
 const ApiService = {
     get(index) {
         return axios
-            .get(`${url}/api/${index}?key=${process.env.NEXT_PUBLIC_API_KEY}`)
+            .get(`${url}/api/${index}?key=${API_KEY}`)
     },
     getGames(id) {
         return axios
-            .get(`${url}/api/games?key=${process.env.NEXT_PUBLIC_API_KEY}&genres=${id}`)
+            .get(`${url}/api/games?key=${API_KEY}&genres=${id}`)
     },
     getGamesDetails(gameId) {
         return axios
-            .get(`${url}/api/games/${gameId}?key=${process.env.NEXT_PUBLIC_API_KEY}`)
+            .get(`${url}/api/games/${gameId}?key=${API_KEY}`)
     },
 
 }
